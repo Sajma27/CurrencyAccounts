ae86f114af4d8603864ff3ba42f3061f199d9116
Sajma27 <szymon.wozniak27@wp.pl>
2022-11-17 10:40:02 +0100

    init

---
+++ src/main/java/com/currency/accounts/CurrencyAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.currency.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyAccountsApplication.class, args);
+	}
+
+}
